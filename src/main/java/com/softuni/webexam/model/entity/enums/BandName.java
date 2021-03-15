644f683a6a915f2355abd196bd9bdb3706815adf
Ivan Panchev <57599673+ivan1981bg@users.noreply.github.com>
2021-03-15 17:22:35 +0200

    Delete src directory

--- src/main/java/com/softuni/webexam/model/entity/enums/BandName.java
+++
@@ -1,5 +0,0 @@
-package com.softuni.webexam.model.entity.enums;
-
-public enum BandName {
-    QUEEN, METALLICA, MADONNA
-}
--- src/main/java/com/softuni/webexam/model/entity/enums/GenreName.java
+++
@@ -1,5 +0,0 @@
-package com.softuni.webexam.model.entity.enums;
-
-public enum GenreName {
-    POP, ROCK, METAL, OTHER
-}
